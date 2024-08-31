1366601de4fd646a187b71e35bb9ae421fa0a2b6
Fanfarium <68463694+Fanfarium@users.noreply.github.com>
2024-08-31 13:59:07 +0300

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -2,6 +2,7 @@ sudo apt update
 sudo apt install ansible -y
 ansible --version
 ansible-galaxy collection install community.docker
+ansible-playbook -i inventory.ini /home/ubuntu/ansibleee/playbook/ping.yml --ask-pass --ask-become-pass
 ansible-playbook -i inventory.ini /home/ubuntu/ansibleee/playbook/docker_and_jenkins.yml
 ansible-playbook -i inventory.ini /home/ubuntu/ansibleee/playbook/kubernetes.yml
 ansible-playbook -i inventory.ini /home/ubuntu/ansibleee/playbook/terraform.yml
