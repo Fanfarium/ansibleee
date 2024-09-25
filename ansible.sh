3c3d312751278f00afde904e642b01ff34ed17c5
Fanfarium <68463694+Fanfarium@users.noreply.github.com>
2024-09-25 20:28:34 +0300

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -5,11 +5,11 @@ apt install sshpass
 apt install python3-pip
 pip install docker-py
 ansible-galaxy collection install community.docker
-ansible-playbook -i ../inventory.ini ping.yml --ask-pass --ask-become-pass
-ansible-playbook -i ../inventory.ini docker.yml --ask-pass --ask-become-pass
-ansible-playbook -i ../inventory.ini jenkins.yml --ask-pass --ask-become-pass
-ansible-playbook -i ../inventory.ini kubernetes.yml --ask-pass --ask-become-pass
-ansible-playbook -i ../inventory.ini terraform.yml --ask-pass --ask-become-pass
+ansible-playbook -i inventory.ini playbook/ping.yml --ask-pass --ask-become-pass
+ansible-playbook -i inventory.ini playbook/docker.yml --ask-pass --ask-become-pass
+ansible-playbook -i inventory.ini playbook/jenkins.yml --ask-pass --ask-become-pass
+ansible-playbook -i inventory.ini playbook/kubernetes.yml --ask-pass --ask-become-pass
+ansible-playbook -i inventory.ini playbook/terraform.yml --ask-pass --ask-become-pass
 
 
 
