518becb6b07a09addf76d5dd6b867e8f1f2409a6
Fanfarium <68463694+Fanfarium@users.noreply.github.com>
2024-09-25 23:24:33 +0300

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -4,11 +4,7 @@ ansible --version
 apt install sshpass
 ansible-galaxy collection install community.docker
 apt update
-ansible-playbook -i inventory.ini playbook/ping.yml --ask-pass --ask-become-pass
-ansible-playbook -i inventory.ini playbook/docker.yml --ask-pass --ask-become-pass
-ansible-playbook -i inventory.ini playbook/jenkins.yml --ask-pass --ask-become-pass
-ansible-playbook -i inventory.ini playbook/kubernetes.yml --ask-pass --ask-become-pass
-ansible-playbook -i inventory.ini playbook/terraform.yml --ask-pass --ask-become-pass
+ansible-playbook -i inventory.ini playbook/ping.yml playbook/docker.yml playbook/jenkins.yml playbook/kubernetes.yml playbook/terraform.yml --ask-pass --ask-become-pass
 
 
 
